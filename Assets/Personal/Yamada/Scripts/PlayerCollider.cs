8675f53efddf6520b536ddf6360f2424fa4beb23
ayato0913a <ayato0913a@gmail.com>
2025-12-22 04:47:21 +0900

    [ update ] Ladderヒット時の処理を追加

--- Assets/Personal/Yamada/Scripts/PlayerCollider.cs
+++ Assets/Personal/Yamada/Scripts/PlayerCollider.cs
@@ -2,5 +2,27 @@ using UnityEngine;
 
 public class PlayerCollider : MonoBehaviour
 {
+    public bool IsLadder => _isLadder;
+    public Ladder CurrentLadder => _currentLadder;
 
+    private bool _isLadder;
+    private Ladder _currentLadder;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ladder"))
+        {
+            _isLadder = true;
+            _currentLadder = collision.gameObject.GetComponent<Ladder>();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ladder"))
+        {
+            _isLadder = false;
+            _currentLadder =null;
+        }
+    }
 }
