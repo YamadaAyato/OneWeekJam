32ea7cdefbff8f629d156d1469a2cf6f6c69013f
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-12-22 02:12:28 +0900

    [update]コメント解除とキューに追加したときのデバッグ追加

--- Assets/Personal/Miyamoto/Scripts/StageManager.cs
+++ Assets/Personal/Miyamoto/Scripts/StageManager.cs
@@ -7,40 +7,42 @@ using UnityEngine;
 public class StageManager : MonoBehaviour
 {
     public int MoveCount => _moveCount;
-    //public Queue<DirectionType> CommandQueue => _commandQueue;
+    public Queue<DirectionType> CommandQueue => _commandQueue;
 
     [SerializeField] private GameObject _player;
     [SerializeField] private Transform _startPos;
     [SerializeField] private int _moveCount;
     [SerializeField] private GameObject _stage;
-    //[serializeField] private PlayerMover OnMoveStarted;
-    //[SerializeField] private Queue<DirectionType> _commandQueue;
+    [SerializeField] private PlayerMover Mover;
+    [SerializeField] private Queue<DirectionType> _commandQueue = new Queue<DirectionType>();
 
     private void Awake()
     {
         Init();
-        Instantiate(_player, _startPos);
+        //Instantiate(_player, _startPos);
     }
     private void OnEnable()
     {
-        //OnMoveStarted += AddCommnd;
+        Mover.OnMoveStarted += AddCommand;
     }
     private void OnDisable()
     {
-        //OnMoveStarted -= AddCommnd;
+        Mover.OnMoveStarted -= AddCommand;
     }
     /// <summary>
     /// 変数を初期化する
     /// </summary>
     private void Init()
     {
-        //_player = FindAnyObjectByType<PlayerController>();
+        _player = FindAnyObjectByType<PlayerInputController>().gameObject;
+        Mover = _player.GetComponent<PlayerMover>();
         //_startPos = GameObject.Find("Start").transform;
         //_moveCount = StageData.MoveCount;
         //_stage = StageData.Stage;
     }
-    //private void AddCommand(DirectionType type)
-    //{
-    //    _commandQueue.Enqueue();
-    //}
+    private void AddCommand(DirectionType type)
+    {
+        _commandQueue.Enqueue(type);
+        Debug.Log($"<color=lime>{type}</color> : をQueueに格納");
+    }
 }
