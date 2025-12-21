6d7aab4deaba4a26935a8d7e90bbeb4d691caa60
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-12-21 22:18:58 +0900

    [add]SEリストとBGMリスト追加

--- Assets/Personal/Miyamoto/Scripts/AudioManager.cs
+++ Assets/Personal/Miyamoto/Scripts/AudioManager.cs
@@ -1,3 +1,5 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
@@ -5,6 +7,7 @@ public class AudioManager : MonoBehaviour
     [System.Serializable]
     public class SoundData
     {
-      
+        [SerializeField] private List<AudioClip> _seList;
+        [SerializeField] private List<AudioClip> _bgmList;
     }
 }
