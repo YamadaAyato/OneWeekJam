906b5a1a5c6550fc808749c7236918fa78d8c4bc
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-12-22 00:23:11 +0900

    [add]ステージの初期化

--- Assets/Personal/Miyamoto/Scripts/StageManager.cs
+++ Assets/Personal/Miyamoto/Scripts/StageManager.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 
+/// <summary>
+/// 全体の行動のマネージャークラス
+/// </summary>
 public class StageManager : MonoBehaviour
 {
     public int MoveCount => _moveCount;
@@ -7,18 +10,21 @@ public class StageManager : MonoBehaviour
     [SerializeField] private GameObject _player;
     [SerializeField] private Transform _startPos;
     [SerializeField] private int _moveCount;
+    [SerializeField] private GameObject _stage;
+    //[SerializeField] private Queue<ICommand> _commandQueue;
 
     private void Awake()
     {
-
+        Init();
     }
     /// <summary>
     /// 変数を初期化する
     /// </summary>
     private void Init()
     {
-        //_player = FindAnyObjectByType<Player>();
+        //_player = FindAnyObjectByType<PlayerController>();
         //_startPos = GameObject.Find("Start").transform;
         //_moveCount = StageData.MoveCount;
+        //_stage = StageData.Stage;
     }
 }
