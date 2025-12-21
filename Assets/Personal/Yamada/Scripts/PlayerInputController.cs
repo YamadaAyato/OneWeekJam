3f2d3162f1d370facd2a240337b59c039f54090b
ayato0913a <ayato0913a@gmail.com>
2025-12-22 01:32:14 +0900

    [ fix ] プレイヤーの入力のswichを修正

--- Assets/Personal/Yamada/Scripts/PlayerInputController.cs
+++ Assets/Personal/Yamada/Scripts/PlayerInputController.cs
@@ -11,21 +11,17 @@ public class PlayerInputController : MonoBehaviour
     {
         if (!_playerMover.IsMoving)
         {
-            switch (_playerMover)
-            {
-                case
-                Input.GetKeyDown(KeyCode.W) => _playerMover.Move(DirectionType.Up);
-                    break;
-                case
-                Input.GetKeyDown(KeyCode.S) => _playerMover.Move(DirectionType.Down);
-                    break;
-                case
-                Input.GetKeyDown(KeyCode.A) => _playerMover.Move(DirectionType.Left);
-                    break;
-                case
-                Input.GetKeyDown(KeyCode.D) => _playerMover.Move(DirectionType.Right);
-                    break;
-            }
+            if (Input.GetKeyDown(KeyCode.W))
+                _playerMover.Move(DirectionType.Up);
+
+            else if (Input.GetKeyDown(KeyCode.S))
+                _playerMover.Move(DirectionType.Down);
+
+            else if (Input.GetKeyDown(KeyCode.A))
+                _playerMover.Move(DirectionType.Left);
+
+            else if (Input.GetKeyDown(KeyCode.D))
+                _playerMover.Move(DirectionType.Right);
         }
     }
 }
