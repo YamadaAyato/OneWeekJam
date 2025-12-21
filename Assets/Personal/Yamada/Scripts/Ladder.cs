24c6009d58841d2a13684cd9414cd0282b0b1b85
ayato0913a <ayato0913a@gmail.com>
2025-12-22 04:45:51 +0900

    [ feat ] 梯子の数となるStepを追加

--- Assets/Personal/Yamada/Scripts/Ladder.cs
+++ Assets/Personal/Yamada/Scripts/Ladder.cs
@@ -2,15 +2,8 @@ using UnityEngine;
 
 public class Ladder : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public int Step => _step;
+    public float CurrentPos => transform.position.y;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private int _step;
 }
