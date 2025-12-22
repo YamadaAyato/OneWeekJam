62b3f5c108c20c35f9be12c152ecca91e76edfb4
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-12-22 17:21:33 +0900

    [update]属性付け忘れ

--- Assets/Personal/Miyamoto/Scripts/StageData.cs
+++ Assets/Personal/Miyamoto/Scripts/StageData.cs
@@ -1,12 +1,17 @@
 using UnityEngine;
 using UnityEngine.UI;
+[System.Serializable]
 public class StageData
 {
     [Tooltip("該当するステージのアウトラインをアサインして")]
     public Outline CurrentStageOutLine;
 
     [Header("インゲームの情報")]
+
+    [Tooltip("ステージのプレファブ")]
     public GameObject StagePrefab;
+    [Tooltip("プレイヤーの行動回数")]
     public int MoveCount;
+    [Tooltip("ステージの番号"), Min(1)]
     public int Index;
 }
