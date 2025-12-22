256ed49670e92c3bbb078d0cea69ffb86eb10829
ayato0913a <ayato0913a@gmail.com>
2025-12-23 06:53:45 +0900

    [ update ] 壊せる床にReset処理追加

--- Assets/Personal/Yamada/Scripts/BreakableFloor.cs
+++ Assets/Personal/Yamada/Scripts/BreakableFloor.cs
@@ -12,6 +12,24 @@ public class BreakableFloor : MonoBehaviour
 
     private Rigidbody2D _rb;
     private BoxCollider2D _boxCollider;
+    private Vector3 _startPos;
+    private StageManager _stageManager;
+    private int _currentPassCount;
+
+    /// <summary>
+    ///         リセット
+    /// </summary>
+    public void ResetFloor()
+    {
+        // 初期化
+        _currentPassCount = _maxPassCount;
+
+        // 落下しているため速度を消す
+        _rb.linearVelocity = Vector2.zero;
+        _rb.bodyType = RigidbodyType2D.Kinematic;
+        _boxCollider.enabled = true;
+        this.transform.position = _startPos;
+    }
 
     /// <summary>
     ///         床を落としながら破壊
@@ -21,15 +39,18 @@ public class BreakableFloor : MonoBehaviour
         // 落ちて消えていくように設定、一定時間たつと破壊
         _rb.bodyType = RigidbodyType2D.Dynamic;
         _boxCollider.enabled = false;
-        Destroy(this.gameObject, _destroyTime);
     }
 
     private void Awake()
     {
+        _stageManager = FindAnyObjectByType<StageManager>();
         _rb = GetComponent<Rigidbody2D>();
         _boxCollider = GetComponent<BoxCollider2D>();
+
         // 通常は動かないように
         _rb.bodyType = RigidbodyType2D.Kinematic;
+        _startPos = this.transform.position;
+        _currentPassCount = _maxPassCount;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -37,11 +58,21 @@ public class BreakableFloor : MonoBehaviour
         if (collision.gameObject.CompareTag("Player")
             || collision.gameObject.CompareTag("Enemy"))
         {
-            _maxPassCount--;
-            Debug.Log($"壊れる床を通過 後{_maxPassCount}回");
+            _currentPassCount--;
+            Debug.Log($"壊れる床を通過 後{_currentPassCount}回");
 
-            if (_maxPassCount <= 0)
+            if (_currentPassCount <= 0)
                 Break();
         }
+    }
+
+    private void OnEnable()
+    {
+        _stageManager.Reset += ResetFloor;
+    }
+
+    private void OnDisable()
+    {
+        _stageManager.Reset -= ResetFloor;
     }
 }
