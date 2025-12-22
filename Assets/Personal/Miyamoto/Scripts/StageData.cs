345ad773e06aef6b968264b6ae74b7a3b7e9dabc
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-12-22 17:18:55 +0900

    [update]スクリプタブルオブジェクト削除のtooltip，Header作成

--- Assets/Personal/Miyamoto/Scripts/StageData.cs
+++ Assets/Personal/Miyamoto/Scripts/StageData.cs
@@ -1,6 +1,12 @@
 using UnityEngine;
-
-public class StageData : ScriptableObject
+using UnityEngine.UI;
+public class StageData
 {
-    
+    [Tooltip("該当するステージのアウトラインをアサインして")]
+    public Outline CurrentStageOutLine;
+
+    [Header("インゲームの情報")]
+    public GameObject StagePrefab;
+    public int MoveCount;
+    public int Index;
 }
