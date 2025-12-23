f303ebc8a98d12cb34dec91b40098d2d58096f8b
ayato0913a <ayato0913a@gmail.com>
2025-12-23 20:39:28 +0900

    [ feat ] 演出などを再生する用のUI一つのクラスを作成　進捗保存

--- Assets/Personal/Yamada/Scripts/ArrowSlotUI.cs
+++ Assets/Personal/Yamada/Scripts/ArrowSlotUI.cs
@@ -3,5 +3,32 @@ using UnityEngine.UI;
 
 public class ArrowSlotUI : MonoBehaviour
 {
+    [SerializeField, Tooltip("このオブジェクトのイメージコンポーネント")] private Image _image;
+    [SerializeField, Tooltip("指示がない状態の空の画像")] private Sprite _emptyCommndImage;
+    private Vector3 _defaultScale;
 
+    public void ResetArrow()
+    {
+        transform.localScale = _defaultScale;
+        _image.sprite = _emptyCommndImage;
+        _image.rectTransform.rotation = Quaternion.identity;
+    }
+
+    public void SetUI(Sprite sprite,float rotationZ)
+    {
+        _image.sprite = sprite;
+        _image.rectTransform.rotation = Quaternion.Euler(0, 0, rotationZ);
+
+        PlayUIEffect();
+    }
+
+    private void PlayUIEffect()
+    {
+        // 演出再生、多分DOTween
+    }
+
+    private void Awake()
+    {
+        _defaultScale = this.transform.localScale;
+    }
 }
