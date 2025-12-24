6f033771f7bbe9244402e57752a8e3e277edd643
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-12-25 01:05:08 +0900

    [feat]変数名変更とプロパティ作成

--- Assets/Personal/Miyamoto/Scripts/StageData.cs
+++ Assets/Personal/Miyamoto/Scripts/StageData.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class StageData
 {
     public int StageId => _stageId;
+    public bool IsCleared => _isCleared;
 
     [Tooltip("該当するステージ")]
     public GameObject SelectStage => _selectStage;
@@ -16,12 +17,12 @@ public class StageData
     [Tooltip("プレイヤーの行動回数")]
     public int MoveCount;
     [Tooltip("ゴールしたかどうかの判定")]
-    private bool _isGoal;
+    private bool _isCleared;
     [Tooltip("ステージの番号")]
     [ReadOnly, SerializeField]
     private int _stageId;
 
     public void SetSelectStage(GameObject stage) => _selectStage = stage;
     public void GetStageID(int value) => _stageId = value;
-    public void SetCleared(bool cleared) => _isGoal = cleared;
+    public void SetCleared(bool cleared) => _isCleared = cleared;
 }
