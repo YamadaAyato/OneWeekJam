2342ce4907210271ce3c7010820c0be1ca917e2b
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-12-25 00:50:38 +0900

    [feat]サマリー追加

--- Assets/Personal/Miyamoto/Scripts/StageProgressManager.cs
+++ Assets/Personal/Miyamoto/Scripts/StageProgressManager.cs
@@ -12,6 +12,11 @@ public static class StageProgressManager : MonoBehaviour
         PlayerPrefs.SetInt(CLEAR_KEY + stageId, 1);
         PlayerPrefs.Save();
     }
+    /// <summary>
+    /// ステージがクリアされているかどうか判断する
+    /// </summary>
+    /// <param name="stageId"></param>
+    /// <returns></returns>
     public bool IsStageCleared(int stageId)
     {
         return PlayerPrefs.GetInt(CLEAR_KEY + stageId, 0) == 1;
