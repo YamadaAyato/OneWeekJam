5f789ab4ea85085b682a489a7ddba239ef21b647
ayato0913a <ayato0913a@gmail.com>
2025-12-25 00:34:11 +0900

    [ feat ] ドッペルゲンガーのコライダーを管理するクラスを作成

---
+++ Assets/Personal/Yamada/Scripts/DoppelgangerColider.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class DoppelgangerColider : MonoBehaviour
+{
+    private bool _isLadder;
+    private Ladder _currentLadder;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ladder"))
+        {
+            _isLadder = true;
+            _currentLadder = collision.gameObject.GetComponent<Ladder>();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ladder"))
+        {
+            _isLadder = false;
+            _currentLadder = null;
+        }
+    }
+}
