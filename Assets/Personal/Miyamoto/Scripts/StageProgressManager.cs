d6421783d6b1476cf5550ff450d3cf7b1b2ed235
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-12-25 00:45:54 +0900

    [feat]サマリー追加

--- Assets/Personal/Miyamoto/Scripts/StageProgressManager.cs
+++ Assets/Personal/Miyamoto/Scripts/StageProgressManager.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 public static class StageProgressManager : MonoBehaviour
 {
     private const string CLEAR_KEY = "STAGE_CLEAR"
+    /// <summary>
+    ///クリア情報をセーブする
+    /// </summary>
+    /// <param name="stageId"></param>
     public void SetStageCleared(int stageId)
     {
         PlayerPrefs.SetInt(CLEAR_KEY + stageId, 1);
