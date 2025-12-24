b16db18425bf9bdfde00adfcae339c96882e6d01
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-12-25 00:57:51 +0900

    [feat]ステージのIDをステージのデータマネージャークラスに持たせる

--- Assets/Personal/Miyamoto/Scripts/StageDataManager.cs
+++ Assets/Personal/Miyamoto/Scripts/StageDataManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 /// </summary>
 public static class StageDataManager
 {
+    public static int StageId;
     /// <summary>行動回数</summary>
     public static int MoveCount;
     /// <summary>ステージプレファブ</summary>
