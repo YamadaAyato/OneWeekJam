c372f8aac85c43b58d599042a479a4f991877e62
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-12-25 02:35:38 +0900

    [delete]MonoBehaviour削除

--- Assets/Personal/Miyamoto/Editor/PlayerPrefsEditor.cs
+++ Assets/Personal/Miyamoto/Editor/PlayerPrefsEditor.cs
@@ -1,16 +1,5 @@
-using UnityEngine;
-
-public class PlayerPrefsEditor : MonoBehaviour
+using UnityEditor;
+public static class PlayerPrefsEditor
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
