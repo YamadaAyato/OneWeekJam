59865abb2e834f4d9fff88b6b25cb6360d0a2752
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-12-25 00:42:58 +0900

    [feat]ステージのIDだけをKeyにすると分かりずらいので定数追加

--- Assets/Personal/Miyamoto/Scripts/StageProgressManager.cs
+++ Assets/Personal/Miyamoto/Scripts/StageProgressManager.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 
 public static class StageProgressManager : MonoBehaviour
 {
+    private const string CLEAR_KEY = "STAGE_CLEAR"
     public void SetStageCleared(int stageId)
     {
-
+        PlayerPrefs.SetInt()
     }
 }
