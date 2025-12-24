95ca04f00abc13765b5fa9a326469872401d833c
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-12-25 02:38:53 +0900

    [feat]データを全リセットする機能実装

--- Assets/Personal/Miyamoto/Editor/PlayerPrefsEditor.cs
+++ Assets/Personal/Miyamoto/Editor/PlayerPrefsEditor.cs
@@ -1,8 +1,12 @@
 using UnityEditor;
+using UnityEngine;
 public static class PlayerPrefsEditor
 {
+    [MenuItem("Tools/PlayerPrefs/Reset")]
     public static void ResetPlayerPrefs()
     {
-
+        PlayerPrefs.DeleteAll();
+        PlayerPrefs.Save();
+        Debug.Log("ステージの情報を全部リセット");
     }
 }
