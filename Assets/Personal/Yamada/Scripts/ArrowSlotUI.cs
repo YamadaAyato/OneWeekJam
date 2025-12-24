6586735d761f797abd3313950cdd4477683a17a4
ayato0913a <ayato0913a@gmail.com>
2025-12-25 04:22:45 +0900

    [ feat ] DOpunchScaleを使って演出を作成

--- Assets/Personal/Yamada/Scripts/ArrowSlotUI.cs
+++ Assets/Personal/Yamada/Scripts/ArrowSlotUI.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,11 +9,17 @@ public class ArrowSlotUI : MonoBehaviour
 {
     [SerializeField, Tooltip("このオブジェクトのイメージコンポーネント")] private Image _image;
     [SerializeField, Tooltip("指示がない状態の空の画像")] private Sprite _emptyCommndImage;
+
+    [Header("演出系")]
+    [SerializeField] private float _effectScale;
+    [SerializeField] private float _effectDuration;
+
+    private Tween _tween;
     private Vector3 _defaultScale;
 
-     /// <summary>
-     ///        向きとイメージをリセットする
-     /// </summary>
+    /// <summary>
+    ///        向きとイメージをリセットする
+    /// </summary>
     public void ResetArrow()
     {
         transform.localScale = _defaultScale;
@@ -25,7 +32,7 @@ public class ArrowSlotUI : MonoBehaviour
     /// </summary>
     /// <param name="sprite"></param>
     /// <param name="rotationZ"></param>
-    public void SetUI(Sprite sprite,float rotationZ)
+    public void SetUI(Sprite sprite, float rotationZ)
     {
         _image.sprite = sprite;
         _image.rectTransform.rotation = Quaternion.Euler(0, 0, rotationZ);
@@ -38,7 +45,11 @@ public class ArrowSlotUI : MonoBehaviour
     /// </summary>
     private void PlayUIEffect()
     {
+        _tween?.Kill();
+
         // 演出再生、多分DOTween
+        _tween = DOTween.Sequence()
+            .Append(transform.DOPunchScale(Vector3.one * _effectScale, _effectDuration, 8, 0.8f));
     }
 
     private void Awake()
