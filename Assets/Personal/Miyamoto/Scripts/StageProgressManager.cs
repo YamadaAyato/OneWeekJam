deedb7b00dd6f592b87a02f216de8c4d1ef2ce1f
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-12-25 00:44:36 +0900

    [feat]クリア情報をセット

--- Assets/Personal/Miyamoto/Scripts/StageProgressManager.cs
+++ Assets/Personal/Miyamoto/Scripts/StageProgressManager.cs
@@ -5,6 +5,6 @@ public static class StageProgressManager : MonoBehaviour
     private const string CLEAR_KEY = "STAGE_CLEAR"
     public void SetStageCleared(int stageId)
     {
-        PlayerPrefs.SetInt()
+        PlayerPrefs.SetInt(CLEAR_KEY + stageId, 1);
     }
 }
