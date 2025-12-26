8c94eded7ddb1abcb6920fa5022ab9a40deba8b4
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-12-27 01:03:26 +0900

    [add]床を開け閉めするスイッチスクリプト

---
+++ Assets/Personal/Miyamoto/Scripts/FloorObject/FloorSwitch.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+
+/// <summary>
+/// 床を開け閉めするスイッチクラス
+/// </summary>
+public class FloorSwitch : MonoBehaviour
+{
+    public event Action OnCharacterEnter;
+    public event Action OnCharacterLeft;
+
+    [Tooltip("ボタン(<color=magenta>突起</color>の部分)のコライダーをアサインしてくれ")]
+    [SerializeField]
+    private Collider2D _buttonCollider;
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<CharacterMoverBase>(out var character))
+        {
+            OnCharacterEnter.Invoke();
+            _animator.SetBool("Push", true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<CharacterMoverBase>(out var character))
+        {
+            OnCharacterLeft.Invoke();
+            _animator.SetBool("Push", false);
+        }
+    }
+}
