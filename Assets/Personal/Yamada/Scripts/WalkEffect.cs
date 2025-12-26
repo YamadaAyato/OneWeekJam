8f315cdf10a11526af5125e1110426da90779cf3
ayato0913a <ayato0913a@gmail.com>
2025-12-27 02:47:04 +0900

    [ feat ] エフェクト実装

--- Assets/Personal/Yamada/Scripts/WalkEffect.cs
+++ Assets/Personal/Yamada/Scripts/WalkEffect.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
 
+/// <summary>
+///         その場に召喚し、時間がたったら消去
+/// </summary>
 public class WalkEffect : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float _lifeTime;
+    [SerializeField]private SpriteRenderer _spriteRenderer;
+
+    /// <summary>
+    ///         向きを変更
+    /// </summary>
+    /// <param name="flipX"></param>
+    public void SetFlip(bool flipX)
     {
-        
+        _spriteRenderer.flipX = flipX;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        Destroy(gameObject, _lifeTime);
     }
 }
