2f5838ff5c9006f174e0a84b80d3a651500ee371
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-12-27 02:35:31 +0900

    [add]テレポータースクリプト

---
+++ Assets/Personal/Miyamoto/Scripts/TeleporterObject/Teleporter.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    [SerializeField]
+    private Teleporter _destination;
+    private Animator _animator;
+    private TeleporterSwitch _teleporterSwitch;
+    private bool _isActive = true;
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _teleporterSwitch = FindAnyObjectByType<TeleporterSwitch>();
+    }
+    private void OnEnable()
+    {
+        _teleporterSwitch.OnTeleported += ReSpwanTeleportor;
+    }
+    private void OnDisable()
+    {
+        _teleporterSwitch.OnTeleported -= ReSpwanTeleportor;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!_isActive) return;
+
+        if (collision.gameObject.TryGetComponent<CharacterMoverBase>(out var charactor))
+        {
+            TeleportedProcces();
+            charactor.transform.position = _destination.transform.position;
+        }
+    }
+    /// <summary>
+    /// テレポートした時の処理
+    /// </summary>
+    private void TeleportedProcces()
+    {
+        //_animator.SetBool("Teleport", true);
+        //_destination._animator.SetBool("Teleport", true);
+        _isActive = false;
+        _destination._isActive = false;
+    }
+    /// <summary>
+    /// テレポートのリスポーン処理
+    /// </summary>
+    public void ReSpwanTeleportor()
+    {
+        _isActive = true;
+        _animator.SetBool("Teleport", false);
+    }
+}
