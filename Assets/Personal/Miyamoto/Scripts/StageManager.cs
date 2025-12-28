baaf9a7941d4150743d8a92e0801fa2ffe8437ab
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-12-28 17:55:32 +0900

    [add]ステージのオーディオ再生

--- Assets/Personal/Miyamoto/Scripts/StageManager.cs
+++ Assets/Personal/Miyamoto/Scripts/StageManager.cs
@@ -25,6 +25,7 @@ public class StageManager : MonoBehaviour
     private void Awake()
     {
         Init();
+        AudioManager.Instance.PlayBGM("Stage");
     }
     private void OnEnable()
     {
