2423926c766ebb3bc9c3453aa51d50a6772e7cdb
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-12-28 14:29:15 +0900

    [feat]テレポータースイッチのアニメーション制御

--- Assets/Personal/Miyamoto/Scripts/TeleporterObject/TeleporterSwitch.cs
+++ Assets/Personal/Miyamoto/Scripts/TeleporterObject/TeleporterSwitch.cs
@@ -3,12 +3,21 @@ using UnityEngine;
 public class TeleporterSwitch : MonoBehaviour
 {
     public event Action OnTeleported;
+    private Animator _animator;
+    private bool _teleported;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.TryGetComponent<CharacterMoverBase>(out var charactor))
+        if (collision.gameObject.TryGetComponent<CharacterMoverBase>(out var charactor) && !_teleported)
         {
             OnTeleported.Invoke();
+            _teleported = true;
+            _animator.SetBool("Push", true);
         }
     }
 }
