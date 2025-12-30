dfd0afaf0267517fe03406c28e29b346e07036de
ayato0913a <ayato0913a@gmail.com>
2025-12-31 07:57:47 +0900

    [ fix ] fadeやプレイヤーが動かないように調整

--- Assets/Personal/Yamada/Scripts/GoalSignalReceiver.cs
+++ Assets/Personal/Yamada/Scripts/GoalSignalReceiver.cs
@@ -1,9 +1,14 @@
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GoalSignalReceiver : MonoBehaviour
 {
+    [SerializeField] private Image _fadePanel;
+    [SerializeField] private float _fadeTime;
     [SerializeField] private string _sceneName;
     private GameObject _player;
+    private Rigidbody2D _rb;
     private Animator _playerAnimator;
 
     public void SetPlayer(GameObject player)
@@ -15,11 +20,20 @@ public class GoalSignalReceiver : MonoBehaviour
     public void HandleDisableControl()
     {
         _player.GetComponent<PlayerInputController>().enabled = false;
+        _rb = _player.GetComponent<Rigidbody2D>();
+        _rb.bodyType = RigidbodyType2D.Kinematic;
+        _rb.linearVelocity = Vector3.zero;
     }
 
     public void HandlePlayGoalAnim()
     {
         _playerAnimator.SetBool("IsGoal", true);
+    }
+
+    public void HandleFade()
+    {
+        _fadePanel.gameObject.SetActive(true);
+        _fadePanel.DOFade(1f, _fadeTime).From(0);
     }
 
     public void HandleFinishGoal()
