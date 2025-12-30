378a2aeffdc5bd0bc546259793fb6db8bc40c593
ayato0913a <ayato0913a@gmail.com>
2025-12-31 08:04:46 +0900

    [ fix ] summaryつけた

--- Assets/Personal/Yamada/Scripts/GoalTimelineController.cs
+++ Assets/Personal/Yamada/Scripts/GoalTimelineController.cs
@@ -2,12 +2,19 @@ using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.Playables;
 
+/// <summary>
+///         ゴール時のタイムライン制御クラス
+/// </summary>
 public class GoalTimelineController : MonoBehaviour
 {
     [SerializeField] private PlayableDirector _director;
     [SerializeField] private CinemachineCamera _zoomCamera;
     [SerializeField] private GoalSignalReceiver _signalReceiver;
 
+    /// <summary>
+    ///         ゴールのタイムライン再生
+    /// </summary>
+    /// <param name="player"></param>
     public void Play(GameObject player)
     {
         // 動的に注入
